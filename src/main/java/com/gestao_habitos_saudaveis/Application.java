245e19658acdf7486ff2c38b4582df6128987d90
Juliana Andrade <juli24013@gmail.com>
2025-12-10 04:19:58 -0300

    Modificando o Application.java

--- src/main/java/com/gestao_habitos_saudaveis/Application.java
+++ src/main/java/com/gestao_habitos_saudaveis/Application.java
@@ -2,8 +2,10 @@ package com.gestao_habitos_saudaveis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan; // Importe esta linha
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gestao_habitos_saudaveis")
 public class Application {
 
 	public static void main(String[] args) {

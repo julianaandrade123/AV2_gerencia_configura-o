4fdaa91918aac56e14f25aee214ac95727281375
Juliana Andrade <juli24013@gmail.com>
2025-12-10 04:28:20 -0300

    Ultima correção

--- src/main/java/com/gestao_habitos_saudaveis/Application.java
+++ src/main/java/com/gestao_habitos_saudaveis/Application.java
@@ -2,9 +2,12 @@ package com.gestao_habitos_saudaveis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan; // Importe esta linha
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Importe esta
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration; // Importe esta
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+// CORREÇÃO FINAL: Exclui a tentativa automática de configurar o DB
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackages = "com.gestao_habitos_saudaveis")
 public class Application {
 
